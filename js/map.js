7a2529387c3d7d44bd9b08ea6e3cd859d75298d5
Lucas Waunn <lwaunn@hawk.iit.edu>
2024-04-19 19:12:02 -0500

    added styling in style.css to make headings appear with map, also centered map on Rowe Village IIT

--- js/map.js
+++ js/map.js
@@ -2,25 +2,24 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
+  // The location of Rowe Village
+  const position = { lat: 41.833710, lng: -87.626209};
+
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 15,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "MAP",
   });
 
-  // The marker, positioned at Uluru
+
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Rowe Village",
   });
 }
 
