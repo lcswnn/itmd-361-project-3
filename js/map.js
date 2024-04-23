c27b2d46b0eb04e31a8db7a8fce2820ae72d6538
Lucas Waunn <lwaunn@hawk.iit.edu>
2024-04-23 14:44:55 -0500

    added marker to map and added bounce animation

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ let map;
 
 async function initMap() {
   // The location of Rowe Village
-  const position = { lat: 41.833710, lng: -87.626209};
+  const rowe = { lat: 41.833710, lng: -87.626209};
 
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
@@ -10,16 +10,20 @@ async function initMap() {
 
   map = new Map(document.getElementById("map"), {
     zoom: 15,
-    center: position,
+    center: rowe,
     mapId: "MAP",
   });
 
-
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Rowe Village",
-  });
+  var marker = new google.maps.Marker({
+		position: rowe,
+		map: map,
+		animation: google.maps.Animation.BOUNCE,
+		icon: {
+      url: 'media/hawk.png',
+      scaledSize: new google.maps.Size(65, 65)
+    }
+    
+	});
 }
 
 initMap();
